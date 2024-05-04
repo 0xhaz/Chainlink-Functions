ddebfdf87c1aad918223eed6447c2f0cfaf62e20
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-05-04 17:35:42 +0800

    initial setup

--- scripts/01_deployConsumers.js
+++ scripts/01_deployConsumers.js
@@ -3,25 +3,15 @@ const { wallet, signer } = require("../connection.js");
 const { networks } = require("../networks.js");
 const { ContractFactory, utils } = require("ethers");
 
-const NETWORK = "polygonMumbai";
+const NETWORK = "ethereumSepolia";
 
 const routerAddress = networks[NETWORK].functionsRouter;
 const donIdBytes32 = utils.formatBytes32String(networks[NETWORK].donId);
 
 const deployFunctionsConsumerContract = async () => {
-  const contractFactory = new ContractFactory(abi, bytecode, wallet);
-
-  console.log(
-    `\nDeploying FunctionsConsumer contract on network ${NETWORK}...`
-  );
-  const functionsConsumerContract = await contractFactory
-    .connect(signer)
-    .deploy(routerAddress, donIdBytes32);
-
-  await functionsConsumerContract.deployed();
-  console.log(`\nDeployed at address ${functionsConsumerContract.address}`)
+  console.log(routerAddress, donIdBytes32);
 };
 
 deployFunctionsConsumerContract().catch(err => {
-  console.log("Error deploying the Consumer Contract ", err);
+  console.log("Error deploying the Consumer Contract: ", err);
 });
